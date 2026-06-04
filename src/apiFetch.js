f16daa0906064d879518bd0e82b9ca35dc173501
schokett <skrollanelenazimmermann@gmail.com>
2026-06-04 15:32:06 +0200

    location paramas api

--- src/apiFetch.js
+++ src/apiFetch.js
@@ -1,12 +1,13 @@
 // fetch
-export async function fetchWeatherData(type) {
+
+export async function fetchWeatherData(type, location = "Nagoya") {
   try {
     const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
     const queryParams = type === "forecast" ? "&days=1" : "&lang=de";
 
     const response = await fetch(
-      `https://api.weatherapi.com/v1/${type}.json?key=${apiKey}&q=köln${queryParams}`,
+      `https://api.weatherapi.com/v1/${type}.json?key=${apiKey}&q=${location}${queryParams}`,
     );
     const data = await response.json();
     return data;
